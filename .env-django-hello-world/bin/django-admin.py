76ea44975f3278ded7ba7904a7ae883f71b61ac3
Samir Azzouz <s.azzouz97@gmail.com>
2020-05-07 17:23:05 +0100

    Started the project

---
+++ .env-django-hello-world/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/szzz/Documents/dev/github/django-hello-world/.env-django-hello-world/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
